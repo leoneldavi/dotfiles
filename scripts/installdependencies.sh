69bf96a45b4bc5c9328a33c918e0fe8970e8a07c
leoneldavi <87317559+leoneldavi@users.noreply.github.com>
2023-01-29 13:22:10 -0300

    Delete scripts directory

--- scripts/installdependencies.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S i3wm i3blocks stow picom dunst feh alacritty awesome-terminal-fonts scrot elementary-icon-theme
-yay -S keyd oldschool-pc-fonts adwaita-dark
-sudo systemctl enable keyd
