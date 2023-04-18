c9fe1734bbdb5cde0650c331191c0839c19d5cb3
leoneldavi <87317559+leoneldavi@users.noreply.github.com>
2023-04-17 22:39:08 -0300

    Stuff i forgor

--- scripts/installoutsidehomefolder.sh
+++ scripts/installoutsidehomefolder.sh
@@ -1,3 +1,4 @@
 sudo cp ~/.dotfiles/etc/keyd/default.conf /etc/keyd/default.conf
 sudo cp ~/.dotfiles/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 sudo cp ~/.dotfiles/etc/i3blocks.conf /etc/i3blocks.conf
+sudo cp ~/.dotfiles/etc/i3blocks.conf /etc/extra-i3blocks.conf
