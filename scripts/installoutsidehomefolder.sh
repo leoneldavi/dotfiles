9ff653c79631b1c637c589f2cf42b5074fb6fb91
leoneldavi <87317559+leoneldavi@users.noreply.github.com>
2023-11-27 13:34:35 -0300

    menos linhas

--- scripts/installoutsidehomefolder.sh
+++ scripts/installoutsidehomefolder.sh
@@ -1,4 +1,1 @@
-sudo cp ~/.dotfiles/etc/keyd/default.conf /etc/keyd/default.conf
-sudo cp ~/.dotfiles/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
-sudo cp ~/.dotfiles/etc/i3blocks.conf /etc/i3blocks.conf
-sudo cp ~/.dotfiles/etc/i3blocks.conf /etc/extra-i3blocks.conf
+sudo cp -r ~/.dotfiles/etc/. /etc/
